9b846777f1831608684900e01a897febf13994dc
Kengo Mori <kenmori@microsoft.com>
2019-07-11 12:34:36 +0900

    Initial Commit

---
+++ GetSP2013WfStatusForList.ps1
@@ -0,0 +1,171 @@
+﻿<#
+ This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment. 
+ THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, 
+ INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.  
+
+ We grant you a nonexclusive, royalty-free right to use and modify the sample code and to reproduce and distribute the object 
+ code form of the Sample Code, provided that you agree: 
+    (i)   to not use our name, logo, or trademarks to market your software product in which the sample code is embedded; 
+    (ii)  to include a valid copyright notice on your software product in which the sample code is embedded; and 
+    (iii) to indemnify, hold harmless, and defend us and our suppliers from and against any claims or lawsuits, including 
+          attorneys' fees, that arise or result from the use or distribution of the sample code.
+
+Please note: None of the conditions outlined in the disclaimer above will supercede the terms and conditions contained within 
+             the Premier Customer Services Description.
+#>
+param(
+ $siteUrl,
+ $listName,
+ $username,
+ $password,
+ $outfile,
+ $resume = $false
+)
+
+Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
+Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.WorkflowServices.dll"
+
+function ExecuteQueryWithIncrementalRetry($retryCount, $delay)
+{
+  $retryAttempts = 0;
+  $backoffInterval = $delay;
+  if ($retryCount -le 0)
+  {
+    throw "Provide a retry count greater than zero."
+  }
+  if ($delay -le 0)
+  {
+    throw "Provide a delay greater than zero."
+  }
+  while ($retryAttempts -lt $retryCount)
+  {
+    try
+    {
+      $script:context.ExecuteQuery();
+      return;
+    }
+    catch [System.Net.WebException]
+    {
+      $response = $_.Exception.Response
+      if ($response -ne $null -and $response.StatusCode -eq 429)
+      {
+        Write-Host ("CSOM request exceeded usage limits. Sleeping for {0} seconds before retrying." -F ($backoffInterval/1000))
+        #Add delay.
+        Start-Sleep -m $backoffInterval
+        #Add to retry count and increase delay.
+        $retryAttempts++;
+        $backoffInterval = $backoffInterval * 2;
+      }
+      else
+      {
+        throw;
+      }
+    }
+  }
+  throw "Maximum retry attempts {0}, have been attempted." -F $retryCount;
+}
+
+function EnumWorkflowsInFolder($list, $ServerRelativeUrl)
+{
+  do
+  {
+    $camlQuery = New-Object Microsoft.SharePoint.Client.CamlQuery
+    $camlQuery.ListItemCollectionPosition = $position
+    $camlQuery.ViewXml = "<View><RowLimit>5000</RowLimit></View>";
+    if ($serverRelativeUrl -ne $null)
+    {
+       $camlQuery.FolderServerRelativeUrl = $ServerRelativeUrl
+    }
+    $listItems = $list.GetItems($camlQuery);
+    $script:context.Load($listItems);
+    ExecuteQueryWithIncrementalRetry -retryCount 5 -delay 30000
+
+    foreach($listItem in $listItems)
+    {
+      if ($listItem.FileSystemObjectType -eq [Microsoft.SharePoint.Client.FileSystemObjectType]::Folder)
+      {
+         $script:context.Load($listItem.Folder)
+         ExecuteQueryWithIncrementalRetry -retryCount 5 -delay 30000
+         EnumWorkflowsInFolder -List $list -ServerRelativeUrl $listItem.Folder.ServerRelativeUrl
+      }
+
+      $wfic = $script:wfis.EnumerateInstancesForListItem($list.Id, $listItem.Id);
+      $script:context.Load($wfic);
+      ExecuteQueryWithIncrementalRetry -retryCount 5 -delay 30000
+ 
+      foreach ($wfi in $wfic)
+      {
+        WriteOut -text (($listItem.Id.ToString()) + "," + (GetWorkflowSubscription -subid $wfi.WorkflowSubscriptionId) + "," + $wfi.Status) -append $true
+        if ($resume)
+        {
+            if ($wfi.Status -eq "Suspended")
+            {
+                $script:wfis.ResumeWorkflow($wfi)
+                ExecuteQueryWithIncrementalRetry -retryCount 5 -delay 30000
+                Write-Output ("Resumed workflow on Item ID = " + $listItem.Id.ToString())
+            }
+        }
+      }
+    } 
+    $position = $listItems.ListItemCollectionPosition
+  }
+  while($position -ne $null)
+}
+
+function GetWorkflowSubscription($subid)
+{
+  if ($script:wfsubhash[$subid.ToString()] -eq $null)
+  {    
+    $sub = $script:wfss.GetSubscription($subid)
+    $script:context.Load($sub)
+    ExecuteQueryWithIncrementalRetry -retryCount 5 -delay 30000
+
+    $script:wfsubhash[$subid.ToString()] = $sub.Name
+    return $sub.Name
+  }
+  else
+  {
+    return $script:wfsubhash[$subid.ToString()]
+  }
+}
+
+function WriteOut($text, $append)
+{
+  if ($outfile -eq $null)
+  {
+    Write-Output $text
+  }
+  else
+  {
+    if ($append)
+    {
+      $text | Out-File $outfile -Append -Encoding UTF8
+    }
+    else
+    {
+      $text | Out-File $outfile -Encoding UTF8
+    }
+  }
+}
+
+$script:context = new-object Microsoft.SharePoint.Client.ClientContext($siteUrl)
+$pwd = convertto-securestring $password -AsPlainText -Force
+$credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, $pwd)
+$script:context.Credentials = $credentials
+
+$wfsm = new-object Microsoft.SharePoint.Client.WorkflowServices.WorkflowServicesManager($script:context, $script:context.Web)
+$script:wfss = $wfsm.GetWorkflowSubscriptionService();
+$script:wfsubhash = @{}
+$script:wfis = $wfsm.GetWorkflowInstanceService();
+
+$list = $script:context.Web.Lists.GetByTitle($listName)
+$script:context.Load($list)
+ExecuteQueryWithIncrementalRetry -retryCount 5 -delay 30000
+
+$wfSubs = $wfss.EnumerateSubscriptionsByList($list.Id);
+$script:context.Load($wfSubs);
+ExecuteQueryWithIncrementalRetry -retryCount 5 -delay 30000
+
+WriteOut -text "ItemId,WorkflowName,Status" 
+EnumWorkflowsInFolder -List $list -serverRelativeUrl $null
---
+++ Readme.md
@@ -0,0 +1,48 @@
+#GetSP2013WfStatusForList 
+
+This is a PowerShell implementation of enumerating the SharePoint 2013 mode workflow for a specified list.
+
+This code has a prerequisite of ShraePoint Online CSOM.
+
+## Prerequitesite
+You need to download SharePoint Online Client Components SDK to run this script.
+https://www.microsoft.com/en-us/download/details.aspx?id=42038
+
+You can also acquire the latest SharePoint Online Client SDK by Nuget as well.
+
+1. You need to access the following site. 
+https://www.nuget.org/packages/Microsoft.SharePointOnline.CSOM
+
+2. Download the nupkg.
+3. Change the file extension to *.zip.
+4. Unzip and extract those file.
+5. place them in the specified directory from the code. 
+
+## How to Run - parameters
+
+-siteUrl ... Target site collection (site) or site (web) URL.
+-listName ... Target List Name (Title)
+-username ... Site Administrator Account to check the workflow instances.
+-password ... The password of the above user.
+-output   ... [optional] When parameter is specified console ouput is saved to file instead.
+-resume   ... [optional] When resume is set to $true, the suspended workflows are resumed when detected.
+
+#### Example 1: Displaying all the workflow instances and their states
+.\GetSP2013WfStatusForList.ps1 -siteUrl https://tenant.sharepoint.com/sites/wf -listName customlist -username admin@tenant.onmicrosoft.com -password PASSWORD
+
+#### Example 2: Writing all the workflow instances and their states to files
+.\GetSP2013WfStatusForList.ps1 -siteUrl https://tenant.sharepoint.com/sites/wf -listName customlist -username admin@tenant.onmicrosoft.com -password PASSWORD -outfile .\output.csv
+
+#### Example 3: Resuming all the suspended instances
+.\GetSP2013WfStatusForList.ps1 -siteUrl https://tenant.sharepoint.com/sites/wf -listName customlist -username admin@tenant.onmicrosoft.com -password PASSWORD -resume $true
+
+
+
+
+
+
+
+
+## Reference
+Original Documentation of this sample code from the following Japan SharePoint Support Team's forum.
+https://blogs.technet.microsoft.com/sharepoint_support/2018/08/10/sample-sharepoint-2013-workflow-status/
